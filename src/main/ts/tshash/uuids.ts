8d2b2ba74a92c5879be69766f473b717acc64b3f
TOGoS <togos00@gmail.com>
2016-04-22 21:50:33 -0500

    Fix type 4 UUIDs to include a '4', not a '3'.

--- src/main/ts/tshash/uuids.ts
+++ src/main/ts/tshash/uuids.ts
@@ -3,8 +3,8 @@ import { hexEncode } from './utils';
 export function newType4Uuid() {
 	const uuid = new Uint8Array(16);
 	for( let i=0; i<16; ++i ) uuid[i] = Math.floor(Math.random()*256);
-	uuid[6] |= 0x30;
-	uuid[6] &= 0x3F;
+	uuid[6] |= 0x40;
+	uuid[6] &= 0x4F;
 	uuid[8] |= 0x80;
 	uuid[8] &= 0xBF;
 	return uuid;
--- src/main/ts/tshash/uuidsTest.ts
+++ src/main/ts/tshash/uuidsTest.ts
@@ -4,7 +4,7 @@ import { newType4Uuid, uuidUrn } from './uuids';
 for( let i=0; i<100; ++i ) {
 	const urn = uuidUrn(newType4Uuid());
 	assertTrue(
-		urn.match(/^urn:uuid:[0-9a-f]{8}-[0-9a-f]{4}-3[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/) != null,
-		"UUID URN should be of the form 'xxxxxxxx-xxxx-3xxx-[89ab]xxx-xxxxxxxxxxxx'; got '"+urn+"'"
+		urn.match(/^urn:uuid:[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/) != null,
+		"UUID URN should be of the form 'xxxxxxxx-xxxx-4xxx-[89ab]xxx-xxxxxxxxxxxx'; got '"+urn+"'"
 	);
 }
